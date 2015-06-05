4f11a2242c45b83500af0eff779ce0e159ead1ff
eladrk <eladkt@gmail.com>
2015-06-05 20:44:17 +0300

    fix angular http access to service

--- public/controllers/shoppingList.js
+++ public/controllers/shoppingList.js
@@ -2,7 +2,7 @@ angular.module('app').controller('listCtrl',listCtrl);
 
 function listCtrl($scope, $http) {
     
-    $http.get('/productList').
+    $http.get('/products/m').
         success(function(data, status, headers, config) {
           $scope.products = data;
         }).
