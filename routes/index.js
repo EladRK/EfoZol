6139ef0816b92247e4d454583c01a8169f987687
eladrk <eladkt@gmail.com>
2015-06-05 13:07:27 +0300

    after bower and angular, working on first rest api response

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/productList', function(req, res, next) {
+
+  res.json(
+    [
+      {name: 'milk', price: 5.5, store: '1' },
+      {name: 'butter', price: 5, store: '1' },
+      {name: 'bread', price: 10, store: '2' },
+      {name: 'spaggety', price: 20, store: '2' },
+      {name: 'oranges', price: 4, store: '3' }
+    ]
+    );
+});
+
+
 module.exports = router;
