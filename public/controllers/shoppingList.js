856b8ef3015f8c216a696e25c155db6f64a8fb6a
eladrk <eladkt@gmail.com>
2015-06-06 11:27:26 +0300

    product list working

--- public/controllers/shoppingList.js
+++ public/controllers/shoppingList.js
@@ -1,13 +1,37 @@
 angular.module('app').controller('listCtrl',listCtrl);
 
 function listCtrl($scope, $http) {
-    
-    $http.get('/products/m').
+
+$scope.newName = "";
+$scope.productsFull = [];
+
+function getProducts(name)
+{
+  if (name === '') {
+    $scope.products = [];
+    return;
+  }
+  
+    $http.get('/products/' + name).
         success(function(data, status, headers, config) {
           $scope.products = data;
         }).
         error(function(data, status, headers, config) {
           // log error
         });
+  
+}
+
+  $scope.$watch("newName", function(newValue, oldValue) {
+   
+    getProducts(newValue);
+  });
+  
+  
+  $scope.addProduct = function (productName) {
+    console.log(productName);
+    $scope.productsFull.push(productName);
+  }
+  
     
 }
