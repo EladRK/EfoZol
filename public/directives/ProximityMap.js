87d540349816400cf3e02ad2cc292c3f2fe58451
eladrk <eladkt@gmail.com>
2015-06-06 14:15:48 +0300

    branches

--- public/directives/ProximityMap.js
+++ public/directives/ProximityMap.js
@@ -1,16 +1,32 @@
 angular.module("app").directive("proximitymap", function() {
 	return {
 		restrict: "E",
-		template: '<map zoom="11"' + 
-			' center="[40.74, -74.18]"' +
-			' dragging-cursor="move"' +
-			' tilt="45"' +
-			' map-type-id="TERRAIN"></map>',
+		templateUrl: 'directives/ProximityMap.html',
 		scope: {
 			title: "@"
 		},
-		controller: function($scope){
+		controller: function($scope, $http){
 			$scope.zoom = 11;
+	
+	// $scope.branches = [
+	// 	{pos: [32.0771929, 34.7870064], name:"elad"},
+	// 	{pos: [32.0898466, 34.7799683], name:"elad"},
+	// 	{pos: [32.0738474, 34.7703552], name:"elad"}
+	// ];
+	
+	
+	  $scope.getBranches = function(){
+      
+    $http.get('/branches').
+      then(function(res) {
+          $scope.branches = res.data;
+      })["catch"](function(err){
+        alert("Error making request", err);
+        console.log(err);
+      });
+  };
+  
+  $scope.getBranches();				
 		}
 	};
 });
--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,27 @@ router.get('/products/:name', function(req, res, next) {
   
 });
 
+router.get('/branches', function(req, res, next) {
+  
+  res.json([
+		{pos: [32.0771929, 34.7870064], name:"סופרסל"},
+		{pos: [32.0898466, 34.7799683], name:"טיב טעם"},
+		{pos: [32.0738474, 34.7703552], name:"AM-PM"}
+	]);
+//   
+//   var search = req.params.name;
+// 
+//   var regex = new RegExp('.*'+search+'.*');
+// 
+//   Product.find({name: regex}, {}).then(function(docs){
+//     res.json(docs);
+//   });
+  
+});
+
+
+
+
+
 
 module.exports = router;
