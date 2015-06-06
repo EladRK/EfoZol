086f9d8e7f065a078d7a296a04203c474b976787
Benjamin Gruenbaum <Benjamin@Benjamins-Macbook-Air.local>
2015-06-06 12:07:09 +0300

    add bootstrap.js file - use this file to fill your local MongoDB collection - it's a good idea to move the shcema to a separate file in the future

---
+++ bootstrap.js
@@ -0,0 +1,36 @@
+var Promise = require("bluebird");
+Promise.longStackTraces();
+var mongoose = Promise.promisifyAll(require('mongoose'));
+
+mongoose.connect('mongodb://localhost:27017/efozol1');
+
+var schema = new mongoose.Schema({ 
+    name: String,
+    price: Number
+});
+
+var Product = mongoose.model('Product', schema);
+
+
+var items = [
+    {"name" : "milk", "price" : 11},
+    {"name" : "butter", "price" : 5},
+    {"name" : "bread", "price" : 4.4},
+    {"name" : "חלב" , "price" : 3.5},
+    {"name" : "גבינה", "price": 4.0}].map(function(item){
+        return new Product({
+            name: item.name,
+            price: item.price
+        });
+     });
+
+Promise.try(function(){
+    return Product.removeAsync({});
+}).then(function(){
+    return Promise.map(items, function(item){
+        return item.saveAsync();
+    });
+}).then(function(){
+    console.log("Done!");
+    process.exit();
+});
