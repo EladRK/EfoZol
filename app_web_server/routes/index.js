5344b525be62bdf59fa37a3da4c30ab472bb6d67
Elad Katz <ekatz@localhost.localdomain>
2015-06-15 20:46:43 +0300

    working with postgres, rest working

--- app_web_server/routes/index.js
+++ app_web_server/routes/index.js
@@ -5,7 +5,47 @@ var router = express.Router();
 var Promise = require("bluebird");
 var mongoose = Promise.promisifyAll(require('mongoose'));
 var Product = require("../models/Product.js");
+
+
+//var Sequelize = require('sequelize');
+//var sequelize = new Sequelize('database', 'username', 'password');
+
+//var sequelize = new Sequelize('postgres://postgres:0o90O(LK@localhost:5432/efozol');
+
+
+var pg = require("pg")
+var conString = "pg://postgres:1234@localhost:5432/efozol";
+var client = new pg.Client(conString);
+client.connect();
+ 
 mongoose.connect('mongodb://localhost:27017/efozol1');
+
+router.get('/products/', function(req, res, next) {
+
+// return sequelize.sync().then(function() {
+//   return User.create({
+//     username: 'janedoe',
+//     birthday: new Date(1980, 6, 20)
+//   });
+// }).then(function(jane) {
+//   console.log(jane.get({
+//     plain: true
+//   }))
+// });
+
+  var query = client.query("SELECT * FROM \"Products\" ORDER BY \"Name\"");
+  query.on("row", function (row, result) {
+    result.addRow(row);
+  });
+  query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(JSON.stringify(result.rows) + "\n");
+    res.end();
+  });
+  
+});
+
 
 router.get('/products/:name', function(req, res, next) {
 
